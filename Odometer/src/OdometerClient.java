7799c302971b3bc6ead4d258b8709ab51692a57a
ipsi16 <ipsita16@hotmail.com>
2015-07-22 16:24:15 +0530

    First Working Program - Finding and printing the initial value

---
+++ Odometer/src/OdometerClient.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Odometer
+{
+	
+	private ArrayList<Integer> valueList = new ArrayList<Integer>();
+	private static int NUM_LENGTH;
+	private static int INIT_VALUE;
+	public Odometer(String inputValue) {
+		NUM_LENGTH = inputValue.length();
+		INIT_VALUE = findInitialValue(NUM_LENGTH);
+	}
+
+	public void printInitialValue()
+	{
+		System.out.println(INIT_VALUE);
+	}
+	public static int findInitialValue(int length)
+	{
+		int init = 0;
+		int count = 1;
+		for(int i = length-1; i>=0; i--)
+		{
+			init += Math.pow(10, i) * (count++);
+		}
+		return init;
+	}
+
+	public static int getIncrement(String value)
+	{
+		
+		
+		return 0;
+		
+	}
+
+}
+public class OdometerClient
+{	
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a value");
+		String inputValue = s.next();
+		Odometer odometerObject = new Odometer(inputValue);
+		odometerObject.printInitialValue();
+	}
+}
